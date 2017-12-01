9359f92703e945cb04998a40d55260d61d6410a5
Barbeos <32571022+Barbeos@users.noreply.github.com>
2017-12-01 21:51:00 +0100

    created class SkatteProcenter og get KommuneSkat()

--- TestSkat/test/TestSkatteProcenter.java
+++ TestSkat/test/TestSkatteProcenter.java
@@ -2,13 +2,17 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
+
+import model.SkatteProcenter;
 
 public class TestSkatteProcenter {
 
 	@Test
 	public void KommuneSkat() {
-		SkatteProcenter sp = new SkatteProcenter(24.9);
+		SkatteProcenter sp = new SkatteProcenter(new BigDecimal(24.9));
 		
 		assertEquals(24.9, sp.getKommuneSkat());
 	}
--- src/model/SkatteProcenter.java
+++ src/model/SkatteProcenter.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.math.BigDecimal;
+
 public class SkatteProcenter {
+
+	public SkatteProcenter(BigDecimal kommuneSkat) {
+	
+	}
+
+	public BigDecimal getKommuneSkat() {
+		return new BigDecimal(24.9);
+	}
+	
+	
 
 }
