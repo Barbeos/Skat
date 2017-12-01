485c1f3b207ee1b02ab2aaddda0aba2ba39fffd9
Barbeos <32571022+Barbeos@users.noreply.github.com>
2017-12-01 21:53:59 +0100

    testKommuneSkat2()

--- TestSkat/test/TestSkatteProcenter.java
+++ TestSkat/test/TestSkatteProcenter.java
@@ -11,10 +11,16 @@ import model.SkatteProcenter;
 public class TestSkatteProcenter {
 
 	@Test
-	public void KommuneSkat() {
+	public void testKommuneSkat() {
 		SkatteProcenter sp = new SkatteProcenter(new BigDecimal(24.9));
 		
 		assertEquals(24.9, sp.getKommuneSkat());
+	}
+	
+	public void testKommuneSkat2() {
+		SkatteProcenter sp = new SkatteProcenter(new BigDecimal(23.9));
+		
+		assertEquals(23.9, sp.getKommuneSkat());
 	}
 
 }
